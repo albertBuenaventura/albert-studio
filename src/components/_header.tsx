1cfbeec5df4d0df5581a2aad44b53e063fda13bd
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2024-02-18 21:21:17 +0800

    refactor: reuseable header component

---
+++ src/components/_header.tsx
@@ -0,0 +1,28 @@
+export default function Header() {
+  return (
+    <>
+      <title>Albert Studio</title>
+      <meta
+        name="description"
+        content="Albert Buenaventura Software Engineer, developer from the Philippines"
+      />
+      <meta property="og:title" content="Albert Studios | Consultant" />
+      <meta property="title" content="Albert Studios | Consultant" />
+      <meta property="og:url" content="https://albertstudios.dev/" />
+      <meta property="url" content="https://albertstudios.dev/" />
+      <meta property="og:site_name" content="Albert Studios" />
+      <meta
+        property="description"
+        content="Albert Buenaventura: Passionate web developer based in the Philippines, specializing in creating innovative digital solutions with expertise in various programming languages and frameworks. Ready to transform your online vision into reality."
+      />
+      <meta name="author" content="Albert Buenaventura" />
+      <meta
+        name="keywords"
+        content="Albert Buenaventura, full stack developer in Philippines, web developer in Philippines, freelance web developer in Philippines"
+      />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+}
