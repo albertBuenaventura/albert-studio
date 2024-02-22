967726246128ba49595bfcc45066ec7e2bd95356
Albert Buenaventura <albert.buenaventura2009@gmail.com>
2024-02-22 22:08:46 +0800

    style: remove dark theme on buttons because its ugly as f

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -18,10 +18,9 @@ export type ButtonProps = {
 const baseClass = "rounded-full h-11 py-2 px-4";
 
 const variantClass = {
-  [ButtonVariant.Primary]:
-    "text-white dark:text-orange-400 bg-orange-600 hover:bg-orange-600/75 dark:bg-slate-800 dark:hover:bg-slate-800/75",
+  [ButtonVariant.Primary]: "text-white bg-orange-600 hover:bg-orange-600/75",
   [ButtonVariant.Secondary]:
-    "bg-transparent text-orange-600 border border-solid border-orange-600 hover:text-white hover:bg-orange-600 dark:bg-slate-800 dark:text-slate-500 dark:border-slate-800 dark:hover:text-white dark:hover:bg-slate-800/50",
+    "bg-transparent text-orange-600 border border-solid border-orange-600 hover:text-white hover:bg-orange-600",
 };
 
 export function Button({
